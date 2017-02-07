ec7678d7798179ea5d2589a8560036a589b6ad50
Robert Sayles <ras570@york.ac.uk>
2017-02-07 13:08:50 +0000

    Updated tile tooltip labels

--- core/src/com/mygdx/game/Tile.java
+++ core/src/com/mygdx/game/Tile.java
@@ -134,18 +134,19 @@ public class Tile extends Button {
         this.ID = ID;
         //Import and save the tile's assigned getID
 
-        tooltipWidth = 155;
-        tooltipHeightLine1 = 29;
-        tooltipHeightLine2 = 21;
-        tooltipHeightTotal = tooltipHeightLine1 + tooltipHeightLine2;
+        tooltipWidth = 275;
+        tooltipHeightLine1 = 26;
+        tooltipHeightLine2 = 18;
         tooltipCursorSpace = 3;
         tooltipTextSpace = 3;
+        tooltipHeightTotal = tooltipHeightLine1 + 2*tooltipHeightLine2 + 2*tooltipTextSpace;
 
         tooltipFillColor = Color.GRAY;
         tooltipLineColor = Color.BLACK;
 
         tooltipFont = new TTFont(Gdx.files.internal("font/testfontbignoodle.ttf"), 36);
         tooltipFont2 = new TTFont(Gdx.files.internal("font/testfontbignoodle.ttf"), 20);
+        tooltipFont2.font().getData().markupEnabled = true;
         //Visual parameters of the tile's tooltip
 
         tooltipActive = false;
@@ -333,17 +334,23 @@ public class Tile extends Button {
      * This must be called during the construction of each frame in which the tooltip is to be shown
      */
     public void drawTooltip() {
+    	int lvls[] = {0,0,0};
+    	if (this.roboticonStored != null) {
+    		lvls = this.roboticonStored.getLevel();
+    	}
         if (tooltipActive == true) {
             if (Gdx.input.getY() < tooltipHeightTotal) {
                 drawer.borderedRectangle(tooltipFillColor, tooltipLineColor, Gdx.input.getX() - tooltipWidth - tooltipCursorSpace, Gdx.input.getY() + tooltipCursorSpace, tooltipWidth, tooltipHeightTotal, 1);
                 //Draw the tooltip's main space onto the screen in the region to the top-left of the cursor
                 drawer.text("Tile " + this.ID, tooltipFont, Gdx.input.getX() - tooltipWidth - tooltipCursorSpace + tooltipTextSpace, Gdx.input.getY() + tooltipCursorSpace + tooltipTextSpace);
-                drawer.text("Food: " + this.FoodCount + " Ore: " + this.OreCount + " Energy: " + this.EnergyCount, tooltipFont2, Gdx.input.getX() - tooltipWidth - tooltipCursorSpace + tooltipTextSpace, Gdx.input.getY() + tooltipHeightLine1 + tooltipCursorSpace + tooltipTextSpace);
+                drawer.text("Base Production: [GREEN]" + this.FoodCount + "[], [RED]" + this.OreCount + "[], [GOLD]" + this.EnergyCount + "[]", tooltipFont2, Gdx.input.getX() - tooltipWidth - tooltipCursorSpace + tooltipTextSpace, Gdx.input.getY() + tooltipHeightLine1 + tooltipCursorSpace + 2*tooltipTextSpace);
+                drawer.text("Current Production: [GREEN]" + this.FoodCount*lvls[2] + "-" + this.FoodCount*lvls[2]*5 + "[], [RED]" + this.OreCount*lvls[0] + "-" + this.OreCount*lvls[0]*5 + "[], [GOLD]" + this.EnergyCount*lvls[1] + "-" + this.EnergyCount*lvls[1]*5 + "[]", tooltipFont2, Gdx.input.getX() - tooltipWidth - tooltipCursorSpace + tooltipTextSpace, Gdx.input.getY() + tooltipHeightLine1 + tooltipHeightLine2 + 2*tooltipCursorSpace + tooltipTextSpace);
                 //Draw an identification label in that space
             } else {
                 drawer.borderedRectangle(tooltipFillColor, tooltipLineColor, Gdx.input.getX() - tooltipWidth - tooltipCursorSpace, Gdx.input.getY() - tooltipHeightTotal - tooltipCursorSpace, tooltipWidth, tooltipHeightTotal, 1);
                 drawer.text("Tile " + this.ID, tooltipFont, Gdx.input.getX() - tooltipWidth - tooltipCursorSpace + tooltipTextSpace, Gdx.input.getY() - tooltipHeightTotal - tooltipCursorSpace + tooltipTextSpace);
-                drawer.text("Food: " + this.FoodCount + " Ore: " + this.OreCount + " Energy: " + this.EnergyCount, tooltipFont2, Gdx.input.getX() - tooltipWidth - tooltipCursorSpace + tooltipTextSpace, Gdx.input.getY() - tooltipHeightLine2 - tooltipCursorSpace + tooltipTextSpace);
+                drawer.text("Base Production: [GREEN]" + this.FoodCount + "[], [RED]" + this.OreCount + "[], [GOLD]" + this.EnergyCount + "[]", tooltipFont2, Gdx.input.getX() - tooltipWidth - tooltipCursorSpace + tooltipTextSpace, Gdx.input.getY() - 2*tooltipHeightLine2 - tooltipCursorSpace);
+                drawer.text("Current Production: [GREEN]" + this.FoodCount*lvls[2] + "-" + this.FoodCount*lvls[2]*5 + "[], [RED]" + this.OreCount*lvls[0] + "-" + this.OreCount*lvls[0]*5 + "[], [GOLD]" + this.EnergyCount*lvls[1] + "-" + this.EnergyCount*lvls[1]*5 + "[]", tooltipFont2, Gdx.input.getX() - tooltipWidth - tooltipCursorSpace + tooltipTextSpace, Gdx.input.getY() - tooltipHeightLine2 - tooltipCursorSpace);
                 //Do the same thing, but in the region to the bottom-left of the cursor if the cursor is near the
                 //top of the game's window
             }
