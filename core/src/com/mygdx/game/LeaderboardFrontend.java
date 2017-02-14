995064abb970cc5994418e56d3eb8531c43b35d9
Steven Tomlinson <smt529@york.ac.uk>
2017-02-14 16:58:03 +0000

    Fixed the displaying of the same name for the leaderboard

--- core/src/com/mygdx/game/LeaderboardFrontend.java
+++ core/src/com/mygdx/game/LeaderboardFrontend.java
@@ -134,8 +134,8 @@ public class LeaderboardFrontend implements Screen {
 
         buttons[0] = new TextButton("Leaderboard", menuButtonStyle);
         buttons[1] = new TextButton(players[0][0] + " : "+players[0][1],menuButtonStyle);
-        buttons[2] = new TextButton(players[0][0] + " : "+players[1][1],menuButtonStyle);
-        buttons[3] = new TextButton(players[0][0] + " : "+players[2][1],menuButtonStyle);
+        buttons[2] = new TextButton(players[1][0] + " : "+players[1][1],menuButtonStyle);
+        buttons[3] = new TextButton(players[2][0] + " : "+players[2][1],menuButtonStyle);
         buttons[4] = new TextButton("Back to Main Menu", menuButtonStyle);
 //        drawer.addTableRow(table, new Label("Leaderboard", new Label.LabelStyle(titleFont.font(), Color.WHITE)), 0, 0, 350, 0);
 //        drawer.addTableRow(table, new Label("Back to Main Menu", new Label.LabelStyle(menuFont.font(), Color.BLACK)), 0, 0, 100, 0);
